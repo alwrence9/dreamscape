72d02f2a5ea06671fa29a307a771ce25cade378e
Farhan Khandaker <farhan.khandaker@dawsoncollege.qc.ca>
2024-02-19 11:50:13 -0500

    Added schema and method to insert profile

--- server/database/db.js
+++ server/database/db.js
@@ -1,5 +1,26 @@
 const mongoose = require('mongoose');
 
+const ProfileSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  }
+});
+const Profile = mongoose.model("Profile", ProfileSchema, "Profiles");
+
+
 class DB{
   constructor(){
     if (!DB.instance){
@@ -21,13 +42,30 @@ class DB{
         throw new Error("Failed to connect to db.")
       }
       this.db = mongoose.connection;
-      console.log("MongoDB connection successful");
+      console.log("MongoDB connection successful.");
     }
   }
 
   async close(){
     mongoose.connection.close()
     console.log("Database connection closed.");
+  }
+
+  async insertProfile({email, password, firstName, lastName}){
+
+    const emailPattern = /^([A-z]|[0-9])+@[A-z]+.com$/g;
+    
+    if(!email.match(emailPattern)){
+      throw new Error("Invalid email.")
+    }
+
+    if(password, firstName, lastName){
+      const newProfile = new Profile({"email": email, "password": password, "firstName": firstName, "lastName": lastName});
+      newProfile.save();
+    }
+    else{
+      throw new Error("Invalid fields for profile.")
+    }
   }
 }
 
