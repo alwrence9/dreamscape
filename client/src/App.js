d402b928c33004f594f1c357fbf5fedbe39ad13f
Hooman Afshari <hooman8097@gmail.com>
2024-02-19 12:08:00 -0500

    Add some hardcode layout

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav>
+          <h1> Triple Z is Cooking!</h1>
+          <ul>
+              <li> Profile </li>
+              <li> Diet </li>
+              <li> Dream </li>
+              <li> Sleep </li>
+              <li> Mentality </li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <section>
+        <h3> Sleepy people image </h3>
+        <h3> Sleepy people image </h3>
+        <h3> Sleepy people image </h3>
+        <h3> Sleepy people image </h3>
+      </section>
+    </>
   );
 }
 
