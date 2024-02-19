faa2953cd75e7f3e035e9944c1896010072486d8
silalis <sila.ben-khelifa@dawsoncollege.qc.ca>
2024-02-19 11:23:31 -0500

    Add general structure of api

--- server/api.js
+++ server/api.js
@@ -4,13 +4,45 @@ const app = express();
 app.use(express.static('../client/build'));
 app.use(express.json());
 
-app.use(express.static('../client/build'));
-
 app.get('/', (req, res)=>{
   res.json({"Soup" : "Soupreme"});
 });
 
-app.use(express.json());
+//Gets the profile of a user for when they login
+app.get('/api/v1/profile', getProfile);
+async function getProfile(req, res) {
+  res.json("hello world");
+}
+
+//Create a profile for a user when they sign up
+app.post('/api/v1/profile', createProfile);
+async function createProfile(req, res) {
+  res.json("hello world");
+}
+
+//Get sleep logs for the week for a specific user
+app.get('/api/v1/sleeplogs/:email', getSleepLogs);
+async function getSleepLogs(req, res) {
+  res.json("hello world");
+}
+
+//Creates a sleep log for a specified user
+app.post('/api/v1/sleeplogs/new/:email', createSleepLog);
+async function createSleepLog(req, res) {
+  res.json("hello world");
+}
+
+//Gets the journal entry for a specified user
+app.get('/api/v1/entries/:email', getEntry);
+async function getEntry(req, res) {
+  res.json("hello world");
+}
+
+//Create a new journal entry for a specified user
+app.get('/api/v1/entries/new/:email', createEntry);
+async function createEntry(req, res) {
+  res.json("hello world");
+}
 
 // 404 route
 app.use((req, res) => {
