ba10681dfb19ba93bb670b4d2035e197dc61733f
silalis <sila.ben-khelifa@dawsoncollege.qc.ca>
2024-02-28 10:34:42 -0500

    Pull from branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation.js';
-import SignupForm from './components/Signup.js'
-import LoginForm from './components/Login.js'
 
 function App() {
   return (
     <>
-      <SignupForm/>
-      <LoginForm/>
+      <Navigation/>
     </>
   );
 }
