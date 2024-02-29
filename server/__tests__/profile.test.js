e62166ad9b6771731f3ffce9bef26983b2e232b6
Farhan Khandaker <farhan.khandaker@dawsoncollege.qc.ca>
2024-02-29 11:36:26 -0500

    Updated test branch

--- server/__tests__/profile.test.js
+++ server/__tests__/profile.test.js
@@ -1,11 +1,41 @@
 const request = require('supertest');
 const { app } = require('../api.js');
-const {DB} = require('../database/db.js');
+const DB = require('../database/db.js');
 
 jest.mock('../database/db.js');
 
-test('Testing', 
-  async () => {
-    expect(1).toEqual(1);
+const ChadrewBrozay = { email: 'chadrew.brozay@example.com', password: "password", firstName: 'Chadrew', lastName: 'Brozay' }
+const GooseGoosersson = { email: 'goose.goosersson@example.com', password: "password", firstName: 'Goose', lastName: 'Goosersson' }
+
+const data = [
+  ChadrewBrozay,
+  GooseGoosersson
+];
+
+describe('GET /api/v1/profile/:email', () => {
+  test('It should respond with user profile for a valid email', async () => {
+    jest.spyOn(DB.prototype, 'getProfile').mockResolvedValue(GooseGoosersson);
+    const response = await request(app).get('/api/v1/profile/goose.goosersson@example.com');
+    expect(response.status).toBe(200);
+    expect(response.body.profile.firstName).toEqual(GooseGoosersson.firstName);
+    expect(response.body.profile.lastName).toEqual(GooseGoosersson.lastName);
+    expect(response.body.profile.email).toEqual(GooseGoosersson.email);
   });
+});
+
+describe('POST /api/v1/profile/new', () => {
+  test('It should create a new profile', async () => {
+    const newProfile = {
+      email: 'new.user@example.com',
+      password: 'password123',
+      firstname: 'New',
+      lastname: 'User',
+    };
+    jest.spyOn(DB.prototype, 'insertProfile').mockImplementation(() => {});
+    const response = await request(app).post('/api/v1/profile/new').send(newProfile);
+    expect(response.status).toBe(201);
+    expect(response.body.message).toBe('Successful');
+
+  });
+});
 
