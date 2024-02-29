c277e5559b0f8e2ee53d157d64421009eb9002b8
Hong Minh Vu <hong.vu@dawsoncollege.qc.ca>
2024-02-28 22:31:29 -0500

    added the test, needed to fix the .env jest but logically it is quite viable

--- server/__tests__/app.tests.js
+++ server/__tests__/app.tests.js
@@ -4,23 +4,13 @@ const { app } = require('../api');
 const DB = require('../database/db')
 jest.mock('../database/db')
 
-
 describe('GET /api/v1/profile/:email', () => {
   test('It should respond with user profile for a valid email', async () => {
-    const mockProfile = { email: 'test@example.com', firstName: 'John', lastName: 'Doe' };
+    const mockProfile = { email: 'test@example.com', firstName: 'Ashley', lastName: 'Vu' };
     jest.spyOn(DB.prototype, 'getProfile').mockResolvedValue(mockProfile);
-
     const response = await request(app).get('/api/v1/profile/test@example.com');
     expect(response.status).toBe(200);
     expect(response.body.profile).toEqual(mockProfile);
-  });
-
-  test('It should handle profile not found', async () => {
-    jest.spyOn(DB.prototype, 'getProfile').mockResolvedValue(null);
-
-    const response = await request(app).get('/api/v1/profile/nonexistent@example.com');
-    expect(response.status).toBe(404);
-    expect(response.body.message).toBe('Profile not found');
   });
 });
 
@@ -32,13 +22,20 @@ describe('POST /api/v1/profile/new', () => {
       firstname: 'New',
       lastname: 'User',
     };
-
     jest.spyOn(DB.prototype, 'insertProfile').mockImplementation(() => {});
-
     const response = await request(app).post('/api/v1/profile/new').send(newProfile);
     expect(response.status).toBe(201);
     expect(response.body.message).toBe('Successful');
-    expect(DB.prototype.insertProfile).toHaveBeenCalledWith(newProfile);
+
   });
 });
-
+describe('GET /api/v1/entries/:email', () => {
+  test('It should respond with dream entries for a valid user', async () => {
+    const mockDreams = [{ date: '2024-02-28', title: 'Dream 1', description: 'Description 1' }];
+    jest.spyOn(require('./database/db.js'), 'getDreamJournals').mockResolvedValue(mockDreams)
+    const response = await request(app).get('/api/v1/entries/test@example.com');
+    expect(response.status).toBe(200);
+    expect(response.body.dreams).toEqual(mockDreams);
+  });
+}
+);
