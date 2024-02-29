2304cad78af721fc567b73f6589ddb5253d9ec3d
Hooman Afshari <hooman8097@gmail.com>
2024-02-29 11:34:05 -0500

    Add exersises videos

--- client/src/components/sleepComponents/Support.js
+++ client/src/components/sleepComponents/Support.js
@@ -5,6 +5,16 @@ function Support() {
     return (
     <>
       <h1> Support </h1>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/aSdc1pKnqDY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/aEqlQvczMJQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/t0kACis_dJE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Us8n8VBQn_c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/5Ju3XvZ6S1Q" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
     </>
   );
 }
