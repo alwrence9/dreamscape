453d47670255fd6003dbdaaf5b776d0d0731c6e7
Hooman Afshari <hooman.afshari@dawsoncollege.qc.ca>
2024-03-02 18:15:15 -0500

    Add gif to home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -5,14 +5,14 @@ function HomePage({ handleSignup, handleLogin }) {
   return (
     <>
       <section>
-        <h3> Sleepy people image </h3>
-        <h3> Sleepy people image </h3>
-        <h3> Sleepy people image </h3>
-        <h3> Sleepy people image </h3>
+        <div id="gif" >       
+          <img src="media/bird.gif" alt="Relaxing Gif" />
+        </div>
         <div className="homeItems">
           <li onClick={() => handleLogin(true)}> Login </li>
           <li onClick={() => handleSignup(true)}> Signup </li>
         </div>
+        <p id="source">GIF Source: <a href="https://www.reddit.com/r/gifs/comments/gnja95/calming_animation_i_did_for_school/"> Click</a></p>
       </section>
     </>
   );
