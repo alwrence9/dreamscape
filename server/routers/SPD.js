9fa7bea453611e8a8e9cfdba6f9bf1349bea9038
Farhan Khandaker <farhan.khandaker@dawsoncollege.qc.ca>
2024-03-14 11:16:14 -0400

    get single spd entry

--- server/routers/SPD.js
+++ server/routers/SPD.js
@@ -5,10 +5,23 @@ const DB = require("../database/db.js");
 const db = new DB();
 
 //Gets the SPD entries
-router.get('/all', getSPDentries);
+router.get('/', getSPDentries);
 async function getSPDentries(req, res) {
   res.type('json');
   const entries = await db.getAllSPD();
+  if(entries){
+    return res.json( {"SPDentries": entries});
+  }
+  else{
+    return res.status(404).send({status: '404', message: "SPD entries not found"});
+  }
+}
+
+//Gets an SPD entry by the ID allocated by Mongo
+router.get('/:id', getSPD);
+async function getSPD(req, res) {
+  res.type('json');
+  const entries = await db.getSPD(req.params.id);
   if(entries){
     return res.json( {"SPDentries": entries});
   }
