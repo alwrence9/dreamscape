f91bb619b994e7e469111e00f71f28eefe339722
unknown <sila.ben-khelifa@dawsoncollege.qc.ca>
2024-03-25 08:14:08 -0400

    Fix bug but still have bugs

--- server/database/tarotSeed.js
+++ server/database/tarotSeed.js
@@ -2,6 +2,7 @@ const DB = require("./db.js");
 const { config } = require('dotenv');
 const path = require('path');
 const fs = require('fs/promises');
+const { BlobServiceClient} = require('@azure/storage-blob');
 
 const currentFile = __filename || typeof require !== 'undefined' && require('url').fileURLToPath || '';
 const currentDirectory = __dirname || path.dirname(currentFile);
@@ -27,6 +28,7 @@ async function init_questions_db(){
 }
 
 async function get_card_images(images_path) {
+  console.log("HEre!")
   const images = [];
   try {
     // Files of directory in array
@@ -37,21 +39,26 @@ async function get_card_images(images_path) {
       const fromPath = path.join( images_path, file );
 
       // To check if files are a directory
-      const stat = await fs.stat( images_path );
+      //const stat = await fs.stat( images_path );
 
-      if( stat.isFile() ) {
+      //if( stat.isFile() ) {
           console.log( "'%s' is a file.", fromPath );
-          const resp = await fetch('/api/v1/images/new', {
-            method : 'POST',
-            headers : {
-            },
-              body: stat
-          })
-          if (resp.ok) {
-            const imageUrl = await resp.json()
-            images.push(imageUrl);
-          }
-        }
+          // const resp = await fetch('localhost:3000/api/v1/images/new', {
+          //   method : 'POST',
+          //   headers : {
+          //   },
+          //     body: stat
+          // })
+          // if (resp.ok) {
+          //   const imageUrl = await resp.json()
+          //   images.push(imageUrl);
+          // }
+          const image =  await fs.readFile(fromPath);
+          const splitPath = fromPath.split("\\");
+          const imageName = splitPath[splitPath.length-1]
+          const imageUrl = await postImage(image, imageName);
+          images.push(imageUrl);
+        //}
     } 
   }
   catch( e ) {
@@ -59,6 +66,32 @@ async function get_card_images(images_path) {
   }
   return images;
 }
+
+const sasToken = process.env.AZURE_SAS;
+const containerName = 'dreamscape';
+const storageAccountName = process.env.storagereousrcename || "azuretest2135666";
+const blobService = new BlobServiceClient(
+  `https://${storageAccountName}.blob.core.windows.net/?${sasToken}`
+);
+const containerClient = blobService.getContainerClient(containerName);
+
+async function postImage(file, fileName) {
+  if (file) {
+    const blobName = fileName
+    const blobClient = containerClient.getBlockBlobClient(blobName);
+
+    //Set mimetype as determined from browser with file upload control
+    const options = { blobHTTPHeaders: { blobContentType: 'application/octet-stream' } };
+    //Upload image to blob storage account in azure
+    await blobClient.uploadData(file, options);
+
+    //Store name & uri into mongodb
+    return JSON.stringify(containerClient.getBlockBlobClient(blobName).url);
+
+    //return res.status(200).send({ status: 200, message: 'Succesful' });
+  }
+}
+
 
 db.connect(dbUrl).then( ()=> init_questions_db() );
 setTimeout(db.close, 3000);
