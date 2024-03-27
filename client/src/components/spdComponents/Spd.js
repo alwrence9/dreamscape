e12205050296dec587c1f57a4c9fbc9f666dec69
Hooman Afshari <hooman8097@gmail.com>
2024-03-27 12:28:18 -0400

    Ensure only logged in user can see spd

--- client/src/components/spdComponents/Spd.js
+++ client/src/components/spdComponents/Spd.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './Spd.css';
 
 function Spd() {
+  const storedToken = localStorage.getItem("token");
+  const email = storedToken ? JSON.parse(storedToken).email : null
+  if (email===null) {
+    return(
+      <h4>You have to login first in order to use this service</h4>
+    );
+  }
+  
   return(
   <>
       <p>SPD</p>
