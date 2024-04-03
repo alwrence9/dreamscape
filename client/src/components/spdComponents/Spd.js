af7e5c206db8a3441ada137b0b49b90effd4076b
Hooman Afshari <hooman8097@gmail.com>
2024-04-03 12:04:21 -0400

    Got map working

--- client/src/components/spdComponents/Spd.js
+++ client/src/components/spdComponents/Spd.js
@@ -1,4 +1,5 @@
 import React , { useState, useEffect } from 'react';
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
 
 import './Spd.css';
 
@@ -85,6 +86,17 @@ function Spd() {
             ))}
       </ul>
     </details>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
   </>
   );
 }
