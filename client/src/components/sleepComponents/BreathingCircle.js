da37ab5efa1d4c72140f01eb231491162810d84a
Hong Minh Vu <hong.vu@dawsoncollege.qc.ca>
2024-04-04 12:45:54 -0400

    Implementing the react native of the brathing circle, still need a lot of modification

--- client/src/components/sleepComponents/BreathingCircle.js
+++ client/src/components/sleepComponents/BreathingCircle.js
@@ -2,4 +2,31 @@
 import { StatusBar } from "expo-status-bar";
 import React, {useRef} from "react";
 import {Stylesheet, Text, View, Dimensions, Animated } from "react-native";
+const {width, height} = Dimensions.get("Window");
 const circleWidth = width / 2;
+const styles = Stylesheet.create({
+  container: {
+    flex:1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    left: width /4,
+    top: height/4
+  }
+})
+export default function BreathingCircle(){
+  return (
+    // <View style = {styles.container }>
+    //   {
+    //   key = {item}
+    //   style = ({
+    //     backgroundColor: "green",
+    //     width:circleWidth,
+    //     height: circleWidth,
+    //     borderRadius: circleWidth /2
+    //   })}
+    // </View>
+    <h1> Implementing the circle here</h1>
+  );
+
+}
