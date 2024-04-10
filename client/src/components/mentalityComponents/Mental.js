f60e3a8f3130bded96996a4893f5aa0403c38476
unknown <sila.ben-khelifa@dawsoncollege.qc.ca>
2024-04-09 21:49:44 -0400

    Edit minor details

--- client/src/components/mentalityComponents/Mental.js
+++ client/src/components/mentalityComponents/Mental.js
@@ -11,18 +11,19 @@ function Mental() {
       <p>
         There are cycles during sleep, these consist of REM and Non-REM sleep. 
         REM sleep is closely related to mentality. 
-
+        <br></br>
+        <br></br>
         During REM (rapid eye movement) stage, the brain processes emotional information. 
         Sleep is important since it is when the brain works on evaluating and remembering thoughts and memories. 
         Without sleep, we would not be able to consolidate positive emotions as well. This is why someone&apos;s 
         mood can be negatively affected with lack of sleep and why mental health can worsen as a result too. 
         <br></br>
         <br></br>
-        Not only that, according to research, there is a bidirectional relationship between sleep and mental health which is why they are said to be closely related. This means that lack of sleep may affect mental health negatively and bad mental health may affect someone&apos;s sleep negatively. Sleeping problems can thus be both a cause and consequence of mental health issues. There is still more research to be done on the topic though
+        Not only that, according to research, there is a bidirectional relationship between sleep and mental health which is why they are said to be closely related. This means that lack of sleep may affect mental health negatively and bad mental health may affect someone&apos;s sleep negatively. Sleeping problems can thus be both a cause and consequence of mental health issues. There is still more research to be done on the topic though.
 
         <br></br>
         <br></br>
-        If there are bad issues for sleep as a result of mental health, or bad mental health as a result of sleep, it is good to consult a professional to try to get to the root of the problem and find a solution for it. Everyone&apos;s situation is different so the best solution can also greatly differ as well
+        If there are bad issues for sleep as a result of mental health, or bad mental health as a result of sleep, it is good to consult a professional to try to get to the root of the problem and find a solution for it. Everyone&apos;s situation is different so the best solution can also greatly differ as well.
       </p>
 
       <h3> Different solutions to improve sleep and mental health include: </h3>
@@ -30,7 +31,7 @@ function Mental() {
       <h4> Controlled deep-breathing </h4>
       <ul>
         <li> By slowing your heart rate, your blood pressure gets reduced and your body becomes more relaxed </li>
-        <li> Different techniques exist for this </li>
+        <li> Different techniques exist for this, some of which may better fit you! </li>
       </ul>
 
       <h4> Meditation </h4>
